25cf8070879fbaa6d3e573df4d028947d6a9493b
zfan2356 <2356754953@qq.com>
2023-11-27 21:51:06 +0800

    init

---
+++ src/com/xingon/Client/common/MessageType.java
@@ -0,0 +1,8 @@
+package com.xingon.Client.common;
+
+public interface MessageType {
+    String MESSAGE_LOGIN_SUCCEED = "1"; // 登录成功
+    String MESSAGE_LOGIN_FAILED = "2"; // 登录失败
+
+
+}
---
+++ src/com/xingon/Server/common/Message.java
@@ -0,0 +1,50 @@
+package com.xingon.Server.common;
+
+public class Message {
+    private static final long serialVersionUID = 1L;
+    private String sender; // 发送方
+    private String receiver; // 接收方
+    private String content; // 消息内容
+    private String sendTime; // 发送时间
+    private String messageType; // 消息类型
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSendTime() {
+        return sendTime;
+    }
+
+    public void setSendTime(String sendTime) {
+        this.sendTime = sendTime;
+    }
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+}
